8e3e9b3a11788935b97e4b365eaa20aea2afca9b
Noah Harman <noahharman190@gmail.com>
2025-10-10 17:26:23 -0400

    Initial Commit

--- BinaryToGrey.v
+++ BinaryToGrey.v
@@ -2,11 +2,7 @@ module binary_to_gray(
   input   [3:0]   bin, 
   output  [3:0]   gray
 );
-  // your code here
-  assign gray[3] = bin[3];
-  
-  always @(*) begin 
-    for(i = 2; 0 < i; i - 1)
-        assign gray[i] = bin[i+1] ^ bin[i];
-  end
+
+  assign gray[3:0] = bin[3:0] ^ {1'b0,bin[3:1]};
+
 endmodule
