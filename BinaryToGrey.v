cd476858f9a8118913dade0d8ea54d3af38c7544
Noah Harman <noahharman190@gmail.com>
2025-10-09 23:39:11 -0400

    Initial Commit

--- BinaryToGrey.v
+++ BinaryToGrey.v
@@ -1,1 +1,10 @@
 //First module
+module binary_to_gray(
+  input   [3:0]   bin, 
+  output  [3:0]   gray
+);
+  // your code here
+  assign gray[0] = bin[1] ^ bin[0];
+  assign gray[3:1] = bin[3:1];
+
+endmodule
