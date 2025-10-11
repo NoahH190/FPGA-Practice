978a898bc59aa7694534f827a2df53698a78f58b
Noah Harman <noahharman190@gmail.com>
2025-10-11 16:11:13 -0400

    Initial Commit

--- GreyToBinary.v
+++ GreyToBinary.v
@@ -1,14 +1,14 @@
 module gray_to_binary(
-  input   [3:0]  gray, 
-  output  [3:0]  binary
+  input [3:0]  gray, 
+  output reg [3:0]  binary
 );
 
   integer i; 
 
-  always@(*) begin 
-    for(i = 3; i >= 0; i = i - 1)begin
-      binary[i] = ^gray[3:i];
-    end
+  always @(*) begin
+    binary[3] = gray[3];
+    for (i = 2; i >= 0; i = i - 1)
+      binary[i] = binary[i+1] ^ gray[i];
   end
 
 endmodule
