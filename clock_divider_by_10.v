b709dc7a2c811bff35c4ab757a3623da0d5b6671
Noah Harman <noahharman190@gmail.com>
2025-10-12 14:53:32 -0400

    Initial Commit

---
+++ clock_divider_by_10.v
@@ -0,0 +1,29 @@
+module clock_div10(
+  input       clk,
+  input       reset,
+  output reg  clk_out
+);
+  // your code here
+
+  integer i;
+
+  always @(posedge clk or posedge reset)
+    if(reset) begin
+      clk_out <= 0;
+      i <= 0;
+    end
+    else begin 
+      if (20 <= i) begin
+        clk_out <= 0;
+        i <= 0;
+      end
+      else if(10 <= i && i < 20) begin
+        clk_out <= 1;
+        i <= i + 1;
+      end 
+      else begin 
+        clk_out <= 0;     
+        i <= i + 1;
+      end
+    end 
+endmodule
