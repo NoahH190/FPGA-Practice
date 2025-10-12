45f1b8025a7e3b23530d103c2467f0f87377f36a
Noah Harman <noahharman190@gmail.com>
2025-10-12 13:27:28 -0400

    Initial Commit

---
+++ clock_divider_by_2.v
@@ -0,0 +1,24 @@
+module clock_div2(
+  input       clk,
+  input       reset,
+  output reg  clk_out
+);
+  // your code here
+
+  integer i;
+
+  always @(posedge clk or posedge reset)
+    if(reset) begin
+      clk_out <= 0;
+      i <= 0;
+    end
+    else begin 
+      if(i % 2 == 0)
+        clk_out <= 1;
+      else 
+        clk_out <= 0;      
+      i <= i + 1;
+    end 
+    
+
+endmodule
